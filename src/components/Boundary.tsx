a71a2c507748138d1f4cad6ec05e0e820ad6eb71
Chanon Roy <chanonroy@gmail.com>
2020-11-28 11:08:22 +0000

    initial commit

---
+++ src/components/Boundary.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react"
+
+const Boundary = (x1: number, y1: number, x2: number, y2: number) => {
+  const [a, setA] = useState([x1, y1])
+  const [b, setB] = useState([x2, y2])
+  
+  return (
+    <div>Boundary</div>
+  )
+}
+
+export default Boundary
