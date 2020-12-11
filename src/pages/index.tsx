034c669c755a386cf75ff23f039ec3fccc3d8c73
Chanon Roy <chanonroy@gmail.com>
2020-12-11 20:11:41 +0000

    feat: add instructions and random wall generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,33 +8,59 @@ import Particle from "../components/Particle"
 import Ray from "../components/Ray"
 import {
   degreesToNormalizedVector,
-  degreesToRadians,
   findLineIntersection,
   rangeMap,
   vectorDistance,
 } from "../math"
 
-const Wrapper = styled.div`
-  display: flex;
+const Background = styled.div`
   height: 100vh;
-  flex-direction: row;
-  justify-content: center;
   align-items: center;
   background: #1e272e;
 `
 
+const GameContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 80px;
+  width: 100%;
+`
+
 const getRays = (x, y, heading): RayType[] => {
   const rays = []
-  for (let a = 0; a < 40; a += 1) {
+  for (let a = -30; a < 30; a += 1) {
     rays.push({ x, y, degrees: a + heading })
   }
   return rays
 }
 
+const generateWalls = (number) => {
+  const randomWalls = []
+  for (let i = 0; i < number; i++) {
+    randomWalls.push({
+      x1: Math.floor(Math.random() * 401),
+      y1: Math.floor(Math.random() * 401),
+      x2: Math.floor(Math.random() * 401),
+      y2: Math.floor(Math.random() * 401),
+    })
+  }
+  return randomWalls
+}
+
 export default function Home() {
-  const [board, setBoard] = useState({
+  const board = {
     width: 400,
     height: 400,
+  }
+
+  const sceneWidth = 400
+  const sceneHeight = 400
+
+  const [walls, setWalls] = useState(generateWalls(3))
+
+  const [particle, setParticle] = useState({
+    x: 20,
+    y: board.height / 2,
   })
 
   const [heading, setHeading] = useState(0)
@@ -43,11 +69,11 @@ export default function Home() {
     const { key } = event
     if (key === "a") {
       // Rotate Left
-      setHeading(heading + 3.5)
+      setHeading(heading - 4)
     }
     if (key === "d") {
       // Rotate right
-      setHeading(heading - 3.5)
+      setHeading(heading + 4)
     }
   }
 
@@ -57,30 +83,6 @@ export default function Home() {
       window.removeEventListener("keydown", handleUserKeyPress)
     }
   }, [handleUserKeyPress])
-
-  const sceneWidth = 400
-  const sceneHeight = 400
-
-  const walls = [
-    {
-      x1: 250,
-      y1: 100,
-      x2: 200,
-      y2: 300,
-    },
-    {
-      x1: 100,
-      y1: 200,
-      x2: 400,
-      y2: 125,
-    },
-  ]
-
-  const [particle, setParticle] = useState({
-    // x: board.width / 2,
-    x: 20,
-    y: 20,
-  })
 
   const onMouseMove = (e: any) => {
     setParticle({
@@ -134,67 +136,85 @@ export default function Home() {
   const { rays: computedRays, scene } = computeRays()
 
   return (
-    <Wrapper>
-      <div style={{ marginLeft: 40 }}>
-        {/* 2D Maze */}
-        <h2 style={{ textAlign: "center", color: "white" }}>2D Render</h2>
-        <Stage
-          onMouseMove={onMouseMove}
-          width={board.width}
-          height={board.height}
-        >
-          <Layer>
-            <Board width={board.width} height={board.height} color="black" />
-          </Layer>
-          <Layer>
-            {walls.map((wall, i) => (
-              <Boundary
-                key={i}
-                start={{ x: wall.x1, y: wall.y1 }}
-                end={{ x: wall.x2, y: wall.y2 }}
-              />
-            ))}
-          </Layer>
-          <Layer>
-            {/* Show rays that eminate around particle */}
-            <Particle position={{ x: particle.x, y: particle.y }} />
-            {!!computedRays.length &&
-              computedRays.map((ray) => {
-                return <Ray key={ray.degrees} ray={ray} />
-              })}
-          </Layer>
-        </Stage>
-      </div>
-
-      {/* 3D Maze */}
-      <div style={{ marginLeft: 40 }}>
-        <h2 style={{ textAlign: "center", color: "white" }}>3D Render</h2>
-        <Stage width={sceneWidth} height={sceneHeight}>
-          <Layer>
-            <Board width={sceneWidth} height={sceneHeight} color="black" />
-          </Layer>
-          <Layer>
-            {scene.map((s, i) => {
-              const renderItemWidth = sceneWidth / scene.length
-              const distanceOpacity = rangeMap(scene[i], 0, sceneWidth, 1, 0)
-              const h = rangeMap(scene[i], 0, sceneWidth, sceneHeight, 0)
-              if (s === Infinity) return
-              return (
-                <Rect
+    <Background>
+      <GameContainer>
+        <div style={{ marginLeft: 40 }}>
+          {/* 2D Maze */}
+          <h2 style={{ textAlign: "center", color: "white" }}>2D Render</h2>
+          <Stage
+            onMouseMove={onMouseMove}
+            width={board.width}
+            height={board.height}
+          >
+            <Layer>
+              <Board width={board.width} height={board.height} color="black" />
+            </Layer>
+            <Layer>
+              {walls.map((wall, i) => (
+                <Boundary
                   key={i}
-                  fill={"white"}
-                  opacity={distanceOpacity}
-                  x={i * renderItemWidth + renderItemWidth / 2}
-                  y={sceneHeight / 10}
-                  strokeEnabled={false}
-                  width={renderItemWidth}
-                  height={h}
+                  start={{ x: wall.x1, y: wall.y1 }}
+                  end={{ x: wall.x2, y: wall.y2 }}
                 />
-              )
-            })}
-          </Layer>
-        </Stage>
+              ))}
+            </Layer>
+            <Layer>
+              {/* Show rays that eminate around particle */}
+              <Particle position={{ x: particle.x, y: particle.y }} />
+              {!!computedRays.length &&
+                computedRays.map((ray) => {
+                  return <Ray key={ray.degrees} ray={ray} />
+                })}
+            </Layer>
+          </Stage>
+        </div>
+
+        {/* 3D Maze */}
+        <div style={{ marginLeft: 40 }}>
+          <h2 style={{ textAlign: "center", color: "white" }}>3D Render</h2>
+          <Stage width={sceneWidth} height={sceneHeight}>
+            <Layer>
+              <Board width={sceneWidth} height={sceneHeight} color="black" />
+            </Layer>
+            <Layer>
+              {scene.map((s, i) => {
+                const renderItemWidth = sceneWidth / scene.length
+                const distanceOpacity = rangeMap(scene[i], 0, sceneWidth, 1, 0)
+                const h = rangeMap(scene[i], 0, sceneWidth, sceneHeight, 0)
+                if (s === Infinity) return
+                return (
+                  <Rect
+                    key={i}
+                    fill={"white"}
+                    opacity={distanceOpacity}
+                    x={i * renderItemWidth + renderItemWidth / 2}
+                    y={sceneHeight / 10}
+                    strokeEnabled={false}
+                    width={renderItemWidth}
+                    height={h}
+                  />
+                )
+              })}
+            </Layer>
+          </Stage>
+        </div>
+      </GameContainer>
+      <strong> </strong>
+      <div
+        style={{
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        <div style={{ marginTop: 40, marginBottom: 20 }}>
+          Press A to look LEFT | Move mouse to MOVE | Press D to look RIGHT
+        </div>
+        <div>
+          <button onClick={() => setWalls(generateWalls(3))}>
+            Random Walls
+          </button>
+        </div>
       </div>
-    </Wrapper>
+    </Background>
   )
 }
